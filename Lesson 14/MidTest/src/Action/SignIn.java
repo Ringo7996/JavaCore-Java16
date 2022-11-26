dfaef57387249fc2794efc3213d1d30ffee8b60f
Ringo7996 <linhnt.jpc@gmail.com>
2022-11-26 21:50:30 +0700

    Update

--- Lesson 14/MidTest/src/Action/SignIn.java
+++ Lesson 14/MidTest/src/Action/SignIn.java
@@ -9,6 +9,52 @@ import java.util.Scanner;
 import static Action.GeneralHandle.*;
 
 public class SignIn {
+    public Account signIn(Scanner scanner, ArrayList<Account> accounts) {
+        Account account;
+        System.out.print("Nhập username:");
+        String username = scanner.nextLine();
+        System.out.print("Nhập password:");
+        String password = scanner.nextLine();
+
+        //Duyệt từng phần tử trong arraylist acc, bắt gặp phần tử trùng username và trùng password thì return acc đó.
+        for (Account i : accounts) {
+            if (username.equals(i.getUsername()) && password.equals(i.getPassword())) {
+                System.out.println("Chào mừng " + i.getUsername() + ", bạn có thể thực hiện các chức năng sau: ");
+                account = i;
+                return account;
+            }
+
+            // nếu có trùng username (ko trùng pw, do trùng pw đã lọc ở if phía trên), thì báo pw ko đúng. Chọn chức năng
+            if (username.equals(i.getUsername())) {
+                System.out.println("Mật khẩu không đúng.");
+                System.out.println("1. Đăng nhập lại \t 2.Quên mật khẩu");
+                System.out.print("Mời chọn chức năng: ");
+                int choice = 0;
+                choice = inputInterger(scanner, choice);
+                while (choice != 1 && choice != 2) {
+                    System.out.print("Vui lòng chọn chức năng hợp lệ: ");
+                    choice = inputInterger(scanner, choice);
+                }
+
+                switch (choice) {
+                    case 1:
+                        account = signIn(scanner, accounts);
+                        return account;
+                    case 2:
+                        ForgotPassword forgotPassword = new ForgotPassword();
+                        account = forgotPassword.forgotPassword(scanner, accounts);
+                        return account;
+                }
+                break;
+            }
+        }
+
+        //Duyệt hết toàn bộ phần tử mà ko có username trùng thì yêu cầu nhập lại.
+        System.out.println("Kiểm tra lại username");
+        return signIn(scanner, accounts);
+    }
+
+
     public void changeUsername(Scanner scanner, ArrayList<Account> accounts, Account account) {
         System.out.print("Nhập username mới: ");
         String newUsername = scanner.nextLine();
--- Lesson 14/MidTest/src/view/MainMenu.java
+++ Lesson 14/MidTest/src/view/MainMenu.java
@@ -22,7 +22,7 @@ public class MainMenu {
         System.out.print("Chọn chức năng: ");
         int choice = 0;
         choice = inputInterger(scanner, choice);
-        while (choice < 1 || choice > 3) {
+        while (choice < 1 || choice > 2) {
             System.out.print("Vui lòng chọn chức năng hợp lệ: ");
             choice = inputInterger(scanner, choice);
         }
@@ -30,7 +30,8 @@ public class MainMenu {
         switch (choice) {
             case 1:
                 // nhập IDPW, tìm đc trong chuỗi thì return acc, ko tìm đc thì nhập lại hoặc tạo acc
-                Account oldAccount = InputIdPw(scanner, accounts);
+                SignIn signIn = new SignIn();
+                Account oldAccount = signIn.signIn(scanner,accounts);
                 signInMenu.signInMenu();
                 signInMenu.signInFunction(scanner, accounts, oldAccount);
 
@@ -42,53 +43,6 @@ public class MainMenu {
                 signInMenu.signInMenu();
                 signInMenu.signInFunction(scanner, accounts, newAccount);
                 break;
-
-            case 3:
-                System.out.println(accounts);
         }
-    }
-
-    public Account InputIdPw(Scanner scanner, ArrayList<Account> accounts) {
-        Account account;
-        System.out.print("Nhập username:");
-        String username = scanner.nextLine();
-        System.out.print("Nhập password:");
-        String password = scanner.nextLine();
-
-        //Duyệt từng phần tử trong arraylist acc, bắt gặp phần tử trùng username và trùng password thì return acc đó.
-        for (Account i : accounts) {
-            if (username.equals(i.getUsername()) && password.equals(i.getPassword())) {
-                System.out.println("Chào mừng " + i.getUsername() + ", bạn có thể thực hiện các chức năng sau: ");
-                account = i;
-                return account;
-            }
-
-            // nếu có trùng username (ko trùng pw, do trùng pw đã lọc ở if phía trên), thì báo pw ko đúng. Chọn chức năng
-            if (username.equals(i.getUsername())) {
-                System.out.println("Mật khẩu không đúng.");
-                System.out.println("1. Đăng nhập lại \t 2.Quên mật khẩu");
-                int choice = 0;
-                choice = inputInterger(scanner, choice);
-                while (choice != 1 && choice != 2) {
-                    System.out.print("Vui lòng chọn chức năng hợp lệ: ");
-                    choice = inputInterger(scanner, choice);
-                }
-
-                switch (choice) {
-                    case 1:
-                        account = InputIdPw(scanner, accounts);
-                        return account;
-                    case 2:
-                        ForgotPassword forgotPassword = new ForgotPassword();
-                        account = forgotPassword.forgotPassword(scanner, accounts);
-                        return account;
-                }
-                break;
-            }
-        }
-
-        //Duyệt hết toàn bộ phần tử mà ko có username trùng thì yêu cầu nhập lại.
-        System.out.println("Kiểm tra lại username");
-        return InputIdPw(scanner, accounts);
     }
 }
